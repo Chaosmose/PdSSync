28e3885c037c8997aaea525e20b7b61abe7c0f02
Benoit PEREIRA DA SILVA <benoit@pereira-da-silva.com>
2014-11-12 16:13:26 +0100

    Sync Admin creation

---
+++ PdSSyncAdmin.h
@@ -0,0 +1,81 @@
+//
+//  PdSSyncAdmin.h
+//  PdSSync
+//
+//  Created by Benoit Pereira da Silva on 12/11/2014.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "PdSSync.h"
+
+@class PdSSyncContext;
+
+
+/**
+ *  Administration interface.
+ */
+@interface PdSSyncAdmin : NSObject
+
+/**
+ *
+ */
+@property (nonatomic,readonly)PdSSyncContext*syncContext;
+
+/**
+ *  Initialize the admin facade with a contzext
+ *
+ *  @param context the context
+ *
+ *  @return the admin instance
+ */
+- (instancetype)initWithContext:(PdSSyncContext*)context;
+
+/**
+ *  Proceed to installation of the Repository
+ *
+ *  @param context the context
+ *  @param block   the completion block
+ */
+- (void)installWithCompletionBlock:(void (^)(BOOL success, NSInteger statusCode))block;
+
+/**
+ *  Creates a tree
+ *
+ *  @param identifier with the given identifier
+ *  @param block      the completion block
+ */
+- (void)createTreeWithId:(NSString*)identifier
+     withCompletionBlock:(void (^)(BOOL success, NSInteger statusCode))block;
+
+/**
+ *  Touches the tree (changes the public ID )
+ *
+ *  @param identifier the tree to be touched
+ *  @param block      the completion block
+ */
+- (void)touchTreeWithId:(NSString*)identifier
+     withCompletionBlock:(void (^)(BOOL success, NSInteger statusCode))block;
+
+/**
+ *  Returns the source and destination hashMaps for a given tree
+ *
+ *  @param identifier the tree identifier
+ *  @param block      the result block 
+ */
+-(void)hashMapsForTreeWithId:(NSString*)identifier
+          withCompletionBlock:(void (^)(HashMap*sourceHashMap,HashMap*destinationHashMap,NSInteger statusCode))block;
+
+
+/**
+ *  Synchronizes the source to the destination
+ *
+ *  @param progressBlock   the progress block
+ *  @param completionBlock the completionBlock 
+ */
+-(void)synchronizeWithprogressBlock:(void(^)(uint taskIndex,float progress))progressBlock
+                 andCompletionBlock:(void(^)(BOOL success,NSString*message))completionBlock;
+
+
+
+@end
